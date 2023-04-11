8198d26f11d8a3bebe3cf8d9ce8e65784a9009b9
powell24 <80400523+powell24@users.noreply.github.com>
2023-04-11 18:37:15 +0800

    Add files via upload

---
+++ p04Age_Payofilin/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p04Age_Payofilin
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			UserAge user1 = new UserAge();
+			Console.WriteLine(user1.getAge());
+		}
+	}
+}
---
+++ p04Age_Payofilin/UserAge.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p04Age_Payofilin
+{
+	internal class UserAge
+	{
+		public int age { get; set; }
+		public string message;
+
+		public UserAge()
+		{
+			age = 0;
+		}
+
+		private void calcAge()
+		{
+			while (true)
+			{
+				Console.Write("Enter your age: ");
+				age = int.Parse(Console.ReadLine());
+				if (age < 13) 
+				{
+					Console.WriteLine("Sorry. Try again");
+					Console.WriteLine();
+				}
+
+				else if (age >= 13)
+				{
+					message = "Welcome!!";
+					break;
+				}
+			}
+		}
+
+		public string getAge()
+		{
+			calcAge();
+			return message; 
+		}
+	}
+}
